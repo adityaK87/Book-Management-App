d8707728a3da9f82cf7006274959d36966eb5708
Aditya kumar <adityapal8763244@gmail.com>
2022-11-29 22:16:43 +0530

    install dependencies to be used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="heading">Namaste World!</h1>
+        <h1 className="heading">Book Management App</h1>
       </header>
     </div>
   );
