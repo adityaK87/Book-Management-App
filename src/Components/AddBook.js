747ce28bfd34f32bc263862481f64e41b70ac8c6
Aditya kumar <adityapal8763244@gmail.com>
2022-12-01 22:53:53 +0530

    getting book

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import BookForm from './BookForm'
 
-const AddBook = () => {
+const AddBook = ({ history, books, setBooks }) => {
     const handleOnSubmit = (book) => {
-        console.log(book)
+        setBooks([book, ...books])
+        history.push('/')
     }
 
     return (
