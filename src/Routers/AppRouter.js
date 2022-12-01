3d9ca46e9734a71eb8a3105bf7060d0e03b50910
Aditya kumar <adityapal8763244@gmail.com>
2022-12-01 22:58:07 +0530

    get book using useLocalStorage hook

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddBook from "../Components/AddBook";
 import BookList from "../Components/BookList";
 import Header from "../Components/Header";
+import UseLocalStorage from "../Hooks/UseLocalStorage";
 
 const AppRouter = () => {
+   const [books, setBook] = UseLocalStorage("books", [])
    return (
       <Router>
          <Header />
          <div>
             <div className="main-content">
                <Routes>
-                  <Route path="/" exact element={<BookList />} />
-                  <Route path="/add" element={<AddBook />} />
+                  <Route path="/" element={<BookList books={books} setBooks={setBook} />} />
+                  {/* <Route path="/add" exact render={(props) =>
+                     <AddBook {...props} books={books} setBooks={setBook} />
+                  } /> */}
+                  <Route path="/add" element={<AddBook books={books} setBooks={setBook} />} />
+
                </Routes>
             </div>
          </div>
