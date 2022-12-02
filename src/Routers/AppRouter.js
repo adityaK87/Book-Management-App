ab929d86f53febdaf39ab7ee900cfb3a9ff0170d
Aditya kumar <adityapal8763244@gmail.com>
2022-12-02 21:38:13 +0530

    create a Route for editing book

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,24 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Navigate,
+} from "react-router-dom";
 import AddBook from "../Components/AddBook";
 import BookList from "../Components/BookList";
+import EditBook from "../Components/EditBook";
 import Header from "../Components/Header";
 import UseLocalStorage from "../Hooks/UseLocalStorage";
 
 const AppRouter = () => {
-   const [books, setBook] = UseLocalStorage("books", [])
+   const [books, setBook] = UseLocalStorage("books", []);
    return (
       <Router>
          <Header />
          <div>
             <div className="main-content">
                <Routes>
-                  <Route path="/" element={<BookList books={books} setBooks={setBook} />} />
+                  <Route
+                     path="/"
+                     element={<BookList books={books} setBooks={setBook} />}
+                  />
                   {/* <Route path="/add" exact render={(props) =>
                      <AddBook {...props} books={books} setBooks={setBook} />
                   } /> */}
-                  <Route path="/add" element={<AddBook books={books} setBooks={setBook} />} />
-
+                  <Route
+                     path="/add"
+                     element={<AddBook books={books} setBooks={setBook} />}
+                  />
+                  <Route
+                     path="/edit/:id"
+                     element={<EditBook books={books} setBooks={setBook} />}
+                  />
+                  <Route element={() => <Navigate path="/" />} />
                </Routes>
             </div>
          </div>
